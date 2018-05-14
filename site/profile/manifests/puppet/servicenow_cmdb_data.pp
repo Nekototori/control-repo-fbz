c3b2f250a8199fc8bf4db4c8a1cdc0136c4c52b1
Jesse Reynolds <jesse@va.com.au>
2018-05-14 15:43:50 +0930

    syntax

--- site/profile/manifests/puppet/servicenow_cmdb_data.pp
+++ site/profile/manifests/puppet/servicenow_cmdb_data.pp
@@ -1,6 +1,6 @@
 class profile::puppet::servicenow_cmdb_data {
 
-  class servicenow_cmdb_data {
+  class { "servicenow_cmdb_data":
     servicenow_cmdb_endpoint => 'https://example.service-now.com/api/now/table/cmdb_ci_server',
   }
 
