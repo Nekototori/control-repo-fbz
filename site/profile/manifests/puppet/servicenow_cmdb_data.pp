7ea5377bcf578b10801555e9974360ff743fa1d4
Jesse Reynolds <jesse@va.com.au>
2018-05-14 15:37:16 +0930

    add servicenow cmdb data test

---
+++ site/profile/manifests/puppet/servicenow_cmdb_data.pp
@@ -0,0 +1,8 @@
+class profile::puppet::servicenow_cmdb_data {
+
+  class servicenow_cmdb_data {
+    servicenow_cmdb_endpoint => 'https://example.service-now.com/api/now/table/cmdb_ci_server',
+  }
+
+}
+
---
+++ site/role/manifests/puppet/master.pp
@@ -0,0 +1,3 @@
+class role::puppet::master {
+  include profile::puppet::servicenow_cmdb_data
+}
