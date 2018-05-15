34c2db777bacb37f0f1ded3cae24dc6edc29d376
Jesse Reynolds <jesse@va.com.au>
2018-05-15 10:02:04 +1000

    set parameters for servicenow_cmdb_data

--- site/profile/manifests/puppet/servicenow_cmdb_data.pp
+++ site/profile/manifests/puppet/servicenow_cmdb_data.pp
@@ -1,7 +1,9 @@
 class profile::puppet::servicenow_cmdb_data {
 
   class { "servicenow_cmdb_data":
-    servicenow_cmdb_endpoint => 'https://example.service-now.com/api/now/table/cmdb_ci_server',
+    servicenow_endpoint => 'https://example.service-now.com/api/now/table/cmdb_ci_server',
+    servicenow_username => 'abc',
+    servicenow_password => '123',
   }
 
 }
