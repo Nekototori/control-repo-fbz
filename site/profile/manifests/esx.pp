0c3aba929533e025fe5fcaee43efdd30fe3725f7
Jesse Reynolds <jesse@va.com.au>
2018-07-06 11:32:57 +1000

    add vcenter test profile

---
+++ site/profile/manifests/esx.pp
@@ -0,0 +1,18 @@
+# @summary esx base profile
+class profiles::esx {
+  include vcenter::package
+
+  transport { 'lab':
+    username => 'root',
+    password => 'root',
+    server   => '192.168.1.1',
+    options  => { 'insecure' => true },
+  }
+
+  vc_datacenter { 'dc1':
+    path      => '/dc1',
+    ensure    => present,
+    transport => Transport['lab'],
+    require   => Transport['lab'],
+  }
+}
