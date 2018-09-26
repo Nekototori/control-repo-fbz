ab2dca759eefd8e4db61a524e7a2b75d995f3863
Jesse Reynolds <jesse@va.com.au>
2018-09-26 12:01:22 +1000

    fix server_list expected data type to string

--- site/profile/manifests/puppet/agent.pp
+++ site/profile/manifests/puppet/agent.pp
@@ -1,6 +1,6 @@
 # Manage the Puppet Agent
 class profile::puppet::agent (
-  Optional[Array] $server_list = undef
+  Optional[String[1]] $server_list = undef
 ) {
 
   # exclude this class on PE infrastructure servers
@@ -8,37 +8,34 @@ class profile::puppet::agent (
 
     include puppet_agent
 
-    if $server_list =~ String[1] {
-
-      ini_setting { 'puppet.conf main server_list':
-        ensure  => present,
-        path    => $facts['puppet_config'],
-        section => 'main',
-        setting => 'server_list',
-        value   => $server_list,
-      }
-
-      ini_setting { 'puppet.conf main server':
-        ensure  => absent,
-        path    => $facts['puppet_config'],
-        section => 'main',
-        setting => 'server',
-      }
-
-      ini_setting { 'puppet.conf agent server_list':
-        ensure  => absent,
-        path    => $facts['puppet_config'],
-        section => 'agent',
-        setting => 'server_list',
-      }
-
-      ini_setting { 'puppet.conf agent server':
-        ensure  => absent,
-        path    => $facts['puppet_config'],
-        section => 'agent',
-        setting => 'server',
-      }
-
+    ini_setting { 'puppet.conf main server_list':
+      ensure  => present,
+      path    => $facts['puppet_config'],
+      section => 'main',
+      setting => 'server_list',
+      value   => $server_list,
     }
+
+    ini_setting { 'puppet.conf main server':
+      ensure  => absent,
+      path    => $facts['puppet_config'],
+      section => 'main',
+      setting => 'server',
+    }
+
+    ini_setting { 'puppet.conf agent server_list':
+      ensure  => absent,
+      path    => $facts['puppet_config'],
+      section => 'agent',
+      setting => 'server_list',
+    }
+
+    ini_setting { 'puppet.conf agent server':
+      ensure  => absent,
+      path    => $facts['puppet_config'],
+      section => 'agent',
+      setting => 'server',
+    }
+
   }
 }
