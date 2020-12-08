45222a3f2acb252db4caff215e38b871e5a00ee5
Jesse Reynolds <jesse@va.com.au>
2020-12-08 11:08:12 +1100

    fix class name

--- site-modules/profile/manifests/puppet/puppet_metrics.pp
+++ site-modules/profile/manifests/puppet/puppet_metrics.pp
@@ -1,5 +1,5 @@
 # sets up metrics collection
-class profile::puppet_metrics {
+class profile::puppet::puppet_metrics {
   include puppet_metrics_collector
   include puppet_metrics_collector::system
 
