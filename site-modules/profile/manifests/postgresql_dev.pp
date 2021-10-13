785eaff9ff192be4104c26162aa9284d665472fb
Jesse Reynolds <jesse@va.com.au>
2021-10-13 13:50:34 +1100

    fix postgres version spec

--- site-modules/profile/manifests/postgresql_dev.pp
+++ site-modules/profile/manifests/postgresql_dev.pp
@@ -1,5 +1,9 @@
 class profile::postgresql_dev () {
-  class { 'postgresql::server':
-    version => '12.8',
+  class { 'postgresql::globals':
+    manage_package_repo => true,
+    version             => '12.8',
   }
+  class { 'postgresql::server':
+  }
+
 }
