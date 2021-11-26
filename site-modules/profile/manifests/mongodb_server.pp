c098c95018423ee9777784069bd328f8f1b4e79b
Jesse Reynolds <jesse@va.com.au>
2021-11-27 00:47:40 +1100

    add mongodb test db and user

--- site-modules/profile/manifests/mongodb_server.pp
+++ site-modules/profile/manifests/mongodb_server.pp
@@ -1,4 +1,11 @@
 #
 class profile::mongodb_server () {
-  include mongodb::server
+  class {'mongodb::server':
+    auth => true,
+  }
+
+  mongodb::db { 'testdb':
+    user          => 'user1',
+    password_hash => 'a15fbfca5e3a758be80ceaf42458bcd8',
+  }
 }
